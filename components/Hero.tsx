46c2fba630163d3401db4e2e5b2b1ce9e079c5da
Volodymyr Korchemnyi <volodymyr.korchemnyi@gmail.com>
2023-07-22 00:52:45 +0300

    fix: wrong div nesting

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,11 +18,11 @@ const Hero = () => {
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
-        <div className="hero__image-container">
-          <div className="hero__image">
-            <Image src="/hero.png" alt="hero" fill className="object-contain" />
-            <div className="hero__image-overlay"></div>
-          </div>
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay"></div>
         </div>
       </div>
     </div>
