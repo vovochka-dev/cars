6dce55ee6675fb6c19ae26f96f94f5474f45710f
Volodymyr Korchemnyi <volodymyr.korchemnyi@gmail.com>
2023-07-22 21:58:52 +0300

    feat: combobox from headlessui in SearchManufacturer

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,70 @@
+'use client';
 import { SearchManufacturerProps } from '@/types';
+import { Combobox, Transition } from '@headlessui/react';
+import Image from 'next/image';
+import { useState, Fragment } from 'react';
+import { manufacturers } from '@/constants';
+
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
-  return <div>SearchManufacturer</div>;
+  const [query, setQuery] = useState('');
+  const filteredManufacturers =
+    query === ''
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/, '')
+            .includes(query.toLowerCase().replace(/\s+/, '')),
+        );
+
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car logo"
+            ></Image>
+          </Combobox.Button>
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          ></Combobox.Input>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options>
+              {filteredManufacturers.map((item) => (
+                <Combobox.Option
+                  key={item}
+                  className={({ active }) => `
+                    relative search-manufacturer__option
+                    ${active ? 'bg-primary-blue text-white' : 'text-gray-900'} 
+                    `}
+                  value={item}
+                >
+                  {item}
+                </Combobox.Option>
+              ))}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;
