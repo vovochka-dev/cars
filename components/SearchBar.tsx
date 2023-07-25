7ea70b81afb2a892fb989cdf9a86a009337ae9f0
Volodymyr Korchemnyi <volodymyr.korchemnyi@gmail.com>
2023-07-25 22:51:06 +0300

    fix: no scroll after router push

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -57,7 +57,9 @@ const SearchBar = () => {
       window.location.pathname
     }?${searchParams.toString()}`;
 
-    router.push(newPathname);
+    router.push(newPathname, {
+      scroll: false,
+    });
   };
 
   return (
