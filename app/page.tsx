cb572d142c2a929a3999d013ffd5915bcdcd3eb3
Volodymyr Korchemnyi <volodymyr.korchemnyi@gmail.com>
2023-07-25 14:51:48 +0300

    feat: first basic implementation of fetchCars

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@ import Image from 'next/image';
 import { Hero } from '@/components';
 import { SearchBar } from '@/components';
 import { CustomFilter } from '@/components';
-export default function Home() {
+import { fetchCars } from '@/utility';
+
+export default async function Home() {
+  const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -18,6 +23,19 @@ export default function Home() {
             <CustomFilter />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => car.model)}
+            </div>
+          </section>
+        ) : (
+          <div>
+            <h2>Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
